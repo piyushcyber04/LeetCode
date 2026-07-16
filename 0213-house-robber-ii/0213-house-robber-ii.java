699a4b8c1f3bfd8d81a149a5d5e64166d5d687ca
Piyush-Tiwari04 <108588284+piyushcyber04@users.noreply.github.com>
2026-07-16 15:37:28 +0530

    Time: 0 ms (100%), Space: 43.2 MB (8.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1){return nums[0];}
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
-        }
-   int []skipfirst=new int[nums.length-1];
-   int []skiplast=new int[nums.length-1];
-   for(int i=0;i<nums.length-1;i++){
-    skipfirst[i]=nums[i+1];
-    skiplast[i]=nums[i];
+        if(nums.length==2){return Math.max(nums[0],nums[1]);}
+int skipfirst =nums.length-1;
+int skiplast=nums.length-1;
+int[]sf=new int[skipfirst];
+int []sl=new int[skiplast];
+for(int i=0;i<nums.length-1;i++){
+    sf[i]=nums[i+1];
+    sl[i]=nums[i];
+}
+  return Math.max(fun(sf),fun(sl));
+}
 
-   }
-   int ans1=fun(skipfirst);
-   int ans2=fun(skiplast);
-   return Math.max(ans1,ans2);
-    }
+
 int fun(int[]arr){
-    int[]dp=new int[arr.length+1];
+    
+    int dp[]=new int[arr.length];
     Arrays.fill(dp,-1);
     dp[0]=arr[0];
     dp[1]=Math.max(arr[0],arr[1]);
     for(int i=2;i<arr.length;i++){
-       
+        if(dp[i]!=-1){
+            return dp[i];
+        }
         dp[i]=Math.max(arr[i]+dp[i-2],dp[i-1]);
     }
-   return dp[dp.length-2];
+    return dp[dp.length-1];
 }
+
 }
